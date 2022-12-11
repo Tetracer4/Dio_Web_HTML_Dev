228e18f6f4cdda1c1776fc3a3936d093f27ef16a
Tetracer4 <nyr.mello@gmail.com>
2022-12-11 16:21:10 +0100

    Food randomness and snake length

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -10,7 +10,7 @@ snake [0] ={
 let direction ="right";
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
-    y:Math.floor(Math.random() * 15 + 1) * box
+    y: Math.floor(Math.random() * 15 + 1) * box
 }
 
 function criarBG(){
@@ -28,7 +28,6 @@ function criarCobrinha (){
 function drawFood(){
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
-
 }
 
 document.addEventListener('keydown', update);
@@ -52,14 +51,19 @@ function iniciarJogo(){
     drawFood();
 
     let snakeX = snake[0].x;
-    let snakeY = snake [0].y;
+    let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop ();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
